47868fda80a09bbc930d269def1e7251c2de474c
Jahiker <rojasjahiker@gmail.com>
2019-11-18 11:59:09 -0400

    bootstrap remote

--- footer.php
+++ footer.php
@@ -1,20 +1,25 @@
-<!-- footer -->
-<div class="container-fluid bg-dark text-white py-3">
-        <div class="container text-center">
-        <blockquote class="blockquote">
-          <a href="https://openweathermap.org/" style="text-decoration: none; color: white;"><p>OpenWeather</p></a>
-          <a href="https://newsapi.org/" style="text-decoration: none; color: white;"><p>News API</p></a>
-          <footer class="blockquote-footer">© Copyright 2019 por <cite title="Source Title">Jahiker Rojas</cite></footer>
-        </blockquote>
-        </div>
-      </div>
-    <!-- footer -->
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="main.js"></script>
-  </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Footer</title>
+</head>
+<body>
+  
+  <!-- footer -->
+  <div class="container-fluid bg-dark text-white py-3">
+    <div class="container text-center">
+    <blockquote class="blockquote">
+      <a href="https://openweathermap.org/" style="text-decoration: none; color: white;"><p>OpenWeather</p></a>
+      <a href="https://newsapi.org/" style="text-decoration: none; color: white;"><p>News API</p></a>
+      <footer class="blockquote-footer">© Copyright 2019 por <cite title="Source Title">Jahiker Rojas</cite></footer>
+    </blockquote>
+    </div>
+  </div>
+  <!-- footer -->
+  
+</body>
 </html>
+
